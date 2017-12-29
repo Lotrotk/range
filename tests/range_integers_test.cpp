767a4e5281e5c52336534d11c91594deddca52b7
Arne Janssens <lotrotk@gmail.com>
2017-12-29 10:22:30 +0100

    preparing tests

--- tests/range_integers_test.cpp
+++ tests/range_integers_test.cpp
@@ -15,13 +15,14 @@ namespace
 	
 	TEST(RangeIntegersTest, CorrectOverlappingRange) {
 		
-		std::array<value_t, 35> const expected
+		std::array<value_t, 53> const expected
 		{
 			value_t{0, 0, 0,},
 			value_t{0, 0, 1,},
 			value_t{0, 0, 2,},
 			value_t{0, 0, 3,},
 			value_t{0, 0, 4,},
+			value_t{0, 0, 5,},
 			value_t{0, 1, 1,},
 			value_t{0, 1, 2,},
 			value_t{0, 1, 3,},
@@ -29,29 +30,46 @@ namespace
 			value_t{0, 2, 2,},
 			value_t{0, 2, 3,},
 			value_t{0, 2, 4,},
+			value_t{0, 2, 5,},
 			value_t{0, 3, 3,},
 			value_t{0, 3, 4,},
 			value_t{0, 4, 4,},
+			value_t{0, 4, 5,},
+			value_t{0, 5, 5,},
 			value_t{1, 1, 1,},
 			value_t{1, 1, 2,},
 			value_t{1, 1, 3,},
 			value_t{1, 1, 4,},
+			value_t{1, 1, 5,},
 			value_t{1, 2, 2,},
 			value_t{1, 2, 3,},
 			value_t{1, 2, 4,},
+			value_t{1, 2, 5,},
 			value_t{1, 3, 3,},
 			value_t{1, 3, 4,},
+			value_t{1, 3, 5,},
 			value_t{1, 4, 4,},
+			value_t{1, 4, 5,},
+			value_t{1, 5, 5,},
 			value_t{2, 2, 2,},
 			value_t{2, 2, 3,},
 			value_t{2, 2, 4,},
+			value_t{2, 2, 5,},
 			value_t{2, 3, 3,},
 			value_t{2, 3, 4,},
 			value_t{2, 4, 4,},
+			value_t{2, 4, 5,},
+			value_t{2, 5, 5,},
 			value_t{3, 3, 3,},
 			value_t{3, 3, 4,},
+			value_t{3, 3, 5,},
 			value_t{3, 4, 4,},
+			value_t{3, 4, 5,},
+			value_t{3, 5, 5,},
 			value_t{4, 4, 4,},
+			value_t{4, 4, 5,},
+			value_t{4, 5, 5,},
+			value_t{5, 5, 5,},
 		};
 		
 		size_t index = 0;
@@ -66,18 +84,31 @@ namespace
 	
 	TEST(RangeIntegersTest, CorrectRange) {
 		
-		std::array<value_t, 10> const expected
+		std::array<value_t, 23> const expected
 		{
 			value_t{0, 1, 2,},
 			value_t{0, 1, 3,},
 			value_t{0, 1, 4,},
 			value_t{0, 2, 3,},
 			value_t{0, 2, 4,},
+			value_t{0, 2, 5,},
 			value_t{0, 3, 4,},
+			value_t{0, 4, 5,},
+			value_t{0, 5, 5,},
 			value_t{1, 2, 3,},
 			value_t{1, 2, 4,},
+			value_t{1, 2, 5,},
 			value_t{1, 3, 4,},
+			value_t{1, 3, 5,},
+			value_t{1, 4, 5,},
+			value_t{1, 5, 5,},
 			value_t{2, 3, 4,},
+			value_t{2, 4, 5,},
+			value_t{2, 5, 5,},
+			value_t{3, 4, 5,},
+			value_t{3, 5, 5,},
+			value_t{4, 5, 5,},
+			value_t{5, 5, 5,},
 		};
 		
 		size_t index = 0;
